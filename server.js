9f1f2c920cd79e80398970a3e5779f9f58776d09
Enrique Lopez <elop22@gmail.com>
2020-11-12 20:22:33 -0700

    added test routes

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
