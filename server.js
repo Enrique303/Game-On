845b7a1c14ca64c33ec0b9c7bd91859313500907
Enrique Lopez <elop22@gmail.com>
2020-11-14 10:03:37 -0700

    switched ports to run concurrently

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
