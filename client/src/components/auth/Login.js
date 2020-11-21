d744e056b43faab9199138aede247bd2ca4334e0
Enrique Lopez <elop22@gmail.com>
2020-11-21 13:56:52 -0700

    styled login and register pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,41 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 const StyledLogin = styled.div`
+  .form .form-group {
+  margin: 1.2rem 0;
+}
 
+.form input[type='email'],
+.form input[type='password'] {
+  display: block;
+  width: 100%;
+  padding: 0.4rem;
+  font-size: 1.2rem;
+  border: 1px solid #ccc;
+}
+.form input[type='submit'] {
+  font: inherit;
+}
+.btn {
+  display: inline-block;
+  background: #f4f4f4;
+  color: #333;
+  padding: 0.4rem 1.3rem;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+  margin-right: 0.5rem;
+  transition: opacity 0.2s ease-in;
+  outline: none;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: hidden;
+  padding: 0 2rem;
+  margin-top: 6rem;
+  margin-bottom: 3rem;
+}
 `;
 
 const Login = () => {
@@ -17,40 +51,42 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    
+
   }
 
   return (
     <StyledLogin>
-      <h1>Sign Into Your Account</h1>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
+      <section className= 'container'>
+        <h1>Sign Into Your Account</h1>
+        <form className='form' onSubmit={e => onSubmit(e)}>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={e => onChange(e)}
+              required />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              minLength='8'
+              value={password}
+              onChange={e => onChange(e)}
+              required />
+          </div>
           <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={e => onChange(e)}
-            required />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            minLength='8'
-            value={password}
-            onChange={e => onChange(e)}
-            required />
-        </div>
-        <input
-          type='submit'
-          className='btn'
-          value='Login' />
-      </form>
-      <p>
-        Don't have an account? <Link to='/register'>Sign up</Link>
-      </p>
+            type='submit'
+            className='btn'
+            value='Login' />
+        </form>
+        <p>
+          Don't have an account? <Link to='/register'>Sign up</Link>
+        </p>
+      </section>
     </StyledLogin>
   )
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -4,7 +4,42 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 const StyledRegister = styled.div`
+  .form .form-group {
+  margin: 1.2rem 0;
+}
 
+.form input[type='text'],
+.form input[type='email'],
+.form input[type='password'] {
+  display: block;
+  width: 100%;
+  padding: 0.4rem;
+  font-size: 1.2rem;
+  border: 1px solid #ccc;
+}
+.form input[type='submit'] {
+  font: inherit;
+}
+.btn {
+  display: inline-block;
+  background: #f4f4f4;
+  color: #333;
+  padding: 0.4rem 1.3rem;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+  margin-right: 0.5rem;
+  transition: opacity 0.2s ease-in;
+  outline: none;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: hidden;
+  padding: 0 2rem;
+  margin-top: 6rem;
+  margin-bottom: 3rem;
+}
 `;
 
 const Register = () => {
@@ -43,54 +78,56 @@ const Register = () => {
 
   return (
     <StyledRegister>
-      <h1>Create An Account</h1>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <div className='form-group'>
+      <section className='container'>
+        <h1>Create An Account</h1>
+        <form className='form' onSubmit={e => onSubmit(e)}>
+          <div className='form-group'>
+            <input
+              type='text'
+              placeholder='Name'
+              name='name'
+              value={name}
+              onChange={ e => onChange(e) }
+              required />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={ e => onChange(e) }
+              required />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              minLength='8'
+              value={password}
+              onChange={ e=> onChange(e) }
+              required />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              name='password2'
+              minLength='8'
+              value={password2}
+              onChange={ e=> onChange(e) }
+              required />
+          </div>
           <input
-            type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={ e => onChange(e) }
-            required />
-        </div>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={ e => onChange(e) }
-            required />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            minLength='8'
-            value={password}
-            onChange={ e=> onChange(e) }
-            required />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
-            minLength='8'
-            value={password2}
-            onChange={ e=> onChange(e) }
-            required />
-        </div>
-        <input
-          type='submit'
-          className='btn'
-          value='Register' />
-      </form>
-      <p>
-        Already have an account? <Link to='/login'>Sign In</Link>
-      </p>
+            type='submit'
+            className='btn'
+            value='Register' />
+        </form>
+        <p>
+          Already have an account? <Link to='/login'>Sign In</Link>
+        </p>
+      </section>  
     </StyledRegister>
   )
 }
