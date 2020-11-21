09aa6ae7837d78994e4f4de2f77d4b091f7d99f4
Enrique Lopez <elop22@gmail.com>
2020-11-21 12:08:58 -0700

    profile get created, error when attempting to update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const Profile = require('../../models/Profile')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 //GET User profile 
 router.get('/myprofile', auth, async (req, res) => {
@@ -16,5 +17,43 @@ router.get('/myprofile', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 });
+
+//Create User Profile
+router.post('/', auth, async (req, res) => {
+  const { bio, location, games, youtube, twitter, twitch, instagram } = req.body
+
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if(bio) profileFields.bio = bio;
+  if(location) profileFields.location = location;
+  if(games) profileFields.games = games;
+
+  profileFields.social ={}
+  if(youtube) profileFields.social.youtube = youtube;
+  if(twitter) profileFields.social.twitter = twitter;
+  if(twitch) profileFields.social.twitch = twitch;
+  if(instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    //Update
+    if (profile) {
+      profile= await Profile.findByIdAndUpdate(
+        { user:req.user.id },
+        { $set:profileFields },
+        { new: true }
+      );
+      return res.json(profile)
+    }
+    profile = new Profile(profileFields)
+    await profile.save();
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 module.exports = router;
